0d370ad49b4efe77a4afa671c7296c66aebd5861
Kewin Dereniewicz <kewindereniewicz@kewins-mbp.ams.nl.ibm.com>
2018-03-15 10:09:35 +0100

    Adding some changes to improve streaming and file handling

--- startStreaming.py
+++ startStreaming.py
@@ -8,10 +8,38 @@
 # to "log4j.rootCategory=WARN, console"
 
 import sys
+import json
+import pandas as pd
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+
+def process_batch(batch):
+    # convert byte lines into tweets jsons
+    # make predictions on tweets
+    # get number of 0's and 1's from predictions
+    # send it to front-end
+    # print("Batch 0:")
+    # print(batch[0])
+    # print("Batch 1:")
+    # print(batch[1])
+    tweet = json.loads(json.loads(batch[1]))
+    #tweet = eval(tweet)
+    #print("Tweet: ")
+    #for key, value in tweet.iteritems():
+        #print("Key: ", key, ", Value: ", value)
+    #print(tweet['followers_count'])
+    #print("********")
+
+    return tweet
+
+
+# Load the classification model
+
+clf = []
+
+# Setup Spark
 
 sc = SparkContext.getOrCreate()#(appName="PythonStreamingDirectKafkaWordCount")
 
@@ -19,7 +47,7 @@ ssc = StreamingContext(sc, 2)
 
 kvs = KafkaUtils.createDirectStream(ssc, ["tweet_stream"],{'metadata.broker.list': "localhost:9092"})
 
-lines = kvs.map(lambda x: x[1])
+lines = kvs.map(lambda x: process_batch(x))
 
 #counts = lines.flatMap(lambda line: line.split(' ')) \
 #              .map(lambda word: (word, 1)) \
@@ -27,6 +55,7 @@ lines = kvs.map(lambda x: x[1])
 
 counts = lines
 
+# Run streaming
 
 counts.pprint()
 ssc.start()
